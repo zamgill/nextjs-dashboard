358e6d3de51ea21b6538004e8c133918b18a66d9
Zamran Gill <zgill@zgconsulting.ca>
2025-09-03 19:16:31 -0400

    fix build error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,11 @@
 import { IBM_Plex_Sans, Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const ibmPlexSans = IBM_Plex_Sans({ subsets: ['latin'] });
+export const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  // below line is required for build
+  weight: ['100', '500'],
+});
 // export const lusitana = Lusitana({
 //   subsets: ['latin'],
 //   weight: ['400', '700'],
